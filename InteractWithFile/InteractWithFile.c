4d3d992bc20f8a08ba5a8e25a1301a8d7bdcc307
Quent <quentin.vincent@efrei.net>
2020-11-23 13:27:10 +0100

    Partie 1 finie !

--- InteractWithFile/InteractWithFile.c
+++ InteractWithFile/InteractWithFile.c
@@ -1,1 +1,81 @@
 #include "InteractWithFile.h"
+
+// prints the main error, for file not found
+void error1(){
+    printf("No such file or directory - error 1\n");
+    exit(1);
+}
+
+// returns the content of a .txt file, given in argument, in a char*
+char* loadFile(FILE* file){
+    int ch;
+    char *text;
+    // if no file, exit
+    if (!file) error1();
+    // first position to 0 / guessed size is 256 (2^8)
+    size_t pos = 0, cap = 256;
+
+    text = malloc((cap)*sizeof(char));
+    // if allocation fails, return NULL
+    if (!text) return NULL;
+    // set the SEEK at the beginning of the file
+    fseek(file, 0, SEEK_SET);
+    // it checks all the chars, until it found the End of File (EOF = -1)
+    while ((ch = fgetc(file)) != EOF) {
+        text[pos] = (char)ch;
+        pos++;
+
+        // if bigger than 256, add 256
+        if (pos >= cap) cap += 256;
+
+        // adapt the size
+        text = realloc(text, (cap+1)*sizeof(char));
+        if (!text) return NULL;
+    }
+    // remove empty allocation
+    text = realloc(text, pos * sizeof(char));
+    if (!text) return NULL;
+
+    text[pos] = '\0';
+    // set the SEEK to the beginning of the file
+    fseek(file, 0, SEEK_SET);
+    return text;
+}
+
+// prints the content given in argument in a .txt file given in argument
+void printFile(FILE* file, char* content){
+    // if no file, exit
+    if (!file) error1();
+    // prints all the content in the file
+    fprintf(file, "%s", content);
+}
+
+// returns the number of chars in a .txt file
+int countCharFile(FILE* file){
+    int count = 0, ch;
+    // if no file, exit
+    if (!file) error1();
+    // set the SEEK to the beginning of the file
+    fseek(file, 0, SEEK_SET);
+    while (1) {
+        ch = fgetc(file);
+        // if End Of File, stop the loop
+        if (ch == EOF) break;
+        count++;
+    }
+    fseek(file, 0, SEEK_SET);
+    return count;
+}
+
+// creates an empty file, or empty the file, with the name given in argument
+void emptyFile(char* name){
+    FILE* file = fopen(name, "w+");
+    // if no file, exit
+    if (!file) error1();
+    char ch[1] = {"\0"};
+    // writes in the empty char \0
+    printFile(file, ch);
+    fclose(file);
+}
+
+
--- InteractWithFile/InteractWithFile.h
+++ InteractWithFile/InteractWithFile.h
@@ -4,4 +4,10 @@
 #ifndef HUFFMAN_INTERACTWITHFILE_H
 #define HUFFMAN_INTERACTWITHFILE_H
 
+void error1(); // prints the main error, for no file found
+char* loadFile(FILE* file); // loads the content of a .txt file, given in argument, in a char*
+void printFile(FILE* file, char* content); // prints the content of a .txt file
+int countCharFile(FILE* file); // returns the number of chars in a .txt file
+void emptyFile(char* name); // create or empty a file
+
 #endif //HUFFMAN_INTERACTWITHFILE_H
