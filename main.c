6ce2a3858da2d23392907050b666ab798c0bd143
Quent <quentin.vincent@efrei.net>
2020-11-26 22:32:51 +0100

    changed output name to binary

--- main.c
+++ main.c
@@ -3,28 +3,28 @@
 #include "Huffman/Coding.h"
 
 int main(void) {
-    FILE *fInput, *fOutput; // files Input.txt and Output.txt
-    size_t cI, cO, ratio = 0; // charIn, charOut, and ratio cO/cI (should be 8, or 0 if empty, so integer)
-    char *contentIn = NULL, *contentOut = NULL; // content of Input.txt and Output.txt
+    FILE *fInput, *fBin; // files input.txt and binary.txt
+    size_t cI, cB, ratio = 0; // charIn, charOut, and ratio cB/cI (should be 8, or 0 if empty, so integer)
+    char *contentIn = NULL, *contentBin = NULL; // content of Input.txt and Output.txt
 
     fInput = fopen("../input.txt", "r+"); // <- Penser à changer le chemin d'acces !!
     if(!fInput) error1(); // if file is not found
-    textFileToBinFile(fInput);
+    textFileToBinFile(fInput, "../binary.txt");
 
-    fOutput = fopen("../output.txt", "r+");
-    if(!fOutput) error1(); // if file is not found
+    fBin = fopen("../binary.txt", "r+");
+    if(!fBin) error1(); // if file is not found
 
     cI = countCharFile(fInput);
-    cO = countCharFile(fOutput);
+    cB = countCharFile(fBin);
     // calculate the ratio, only if cI != 0
-    if (cI) ratio = cO/cI;
+    if (cI) ratio = cB/cI;
 
     contentIn = loadFile(fInput);
-    contentOut = loadFile(fOutput);
+    contentBin = loadFile(fBin);
 
-    printf("\nInput : %s\nOutput : %s\n\nInput #char : %d\nOutput #char : %d\nRatio : %d\n", contentIn, contentOut, cI, cO, ratio);
+    printf("\nInput : %s\nOutput : %s\n\nInput #char : %d\nOutput #char : %d\nRatio : %d\n", contentIn, contentBin, cI, cB, ratio);
 
     fclose(fInput);
-    fclose(fOutput);
+    fclose(fBin);
     return EXIT_SUCCESS;
 }
