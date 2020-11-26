42b6e87a88ca40a3b1c4f79ad7a0ab834324081a
Quent <quentin.vincent@efrei.net>
2020-11-26 21:46:10 +0100

    upgraded memory allocation

--- InteractWithFile/InteractWithFile.c
+++ InteractWithFile/InteractWithFile.c
@@ -12,10 +12,10 @@ char* loadFile(FILE* file){
     char *text;
     // if no file, exit
     if (!file) error1();
-    // first position to 0 / guessed size is 256 (2^8)
-    size_t pos = 0, size = 256;
+    // first position to 0 / guessed size is 8 (2^3)
+    size_t pos = 0, cap = 8;
 
-    text = malloc((size)*sizeof(char));
+    text = malloc(cap*sizeof(char));
     // if allocation fails, return NULL
     if (!text) return NULL;
     // set the SEEK at the beginning of the file
@@ -25,10 +25,12 @@ char* loadFile(FILE* file){
         text[pos] = (char)ch;
         pos++;
 
-        if (pos >= size) size += 256;
-
-        // adapt the size
-        text = realloc(text, (size+1)*sizeof(char));
+        // if bigger than 8, add 8
+        if (pos >= cap) {
+            cap += 8;
+            // adapt the size
+            text = realloc(text, cap * sizeof(char));
+        }
         if (!text) return NULL;
     }
     // remove empty allocation
@@ -36,23 +38,29 @@ char* loadFile(FILE* file){
     if (!text) return NULL;
 
     text[pos] = '\0';
+    // set the SEEK to the beginning of the file
     fseek(file, 0, SEEK_SET);
     return text;
 }
 
 // prints the content given in argument in a .txt file given in argument
 void printFile(FILE* file, char* content){
+    // if no file, exit
     if (!file) error1();
+    // prints all the content in the file
     fprintf(file, "%s", content);
 }
 
 // returns the number of chars in a .txt file
 int countCharFile(FILE* file){
     int count = 0, ch;
+    // if no file, exit
     if (!file) error1();
+    // set the SEEK to the beginning of the file
     fseek(file, 0, SEEK_SET);
     while (1) {
         ch = fgetc(file);
+        // if End Of File, stop the loop
         if (ch == EOF) break;
         count++;
     }
@@ -63,11 +71,10 @@ int countCharFile(FILE* file){
 // creates an empty file, or empty the file, with the name given in argument
 void emptyFile(char* name){
     FILE* file = fopen(name, "w+");
+    // if no file, exit
     if (!file) error1();
     char ch[1] = {"\0"};
     // writes in the empty char \0
     printFile(file, ch);
     fclose(file);
 }
-
-
