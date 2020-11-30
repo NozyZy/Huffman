838c066bfc46d70b236c3a69001c9d16eab9cea2
Quent <quentin.vincent@efrei.net>
2020-11-30 19:04:06 +0100

    partie 2.2 tests :white_check_mark:

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include "Huffman/Coding.h"
 
 int main(void) {
-    printf("oui");
+    FILE* fInput;
+    fInput = fopen("../input.txt", "r+");
+    if (!fInput) error1();
+
+    char* content = loadFile(fInput);
+    Element* l = NULL;
+    l = creerListeOccurrences(content, countCharFile(fInput));
+    afficherListe("la liste", l);
+
+    Noeud* a = NULL;
+    a = creeArbreOccurrences(l);
+    afficherArbre(a);
+
+    freeList(l);
+    freeArbre(a);
 
     return EXIT_SUCCESS;
 }
