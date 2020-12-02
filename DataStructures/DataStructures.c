7c73dc0024d5557092ae3dd1d9b77d9ab3a47cb6
Valère <61429521+ValereG@users.noreply.github.com>
2020-12-02 20:20:08 +0100

    Update DataStructures.c

--- DataStructures/DataStructures.c
+++ DataStructures/DataStructures.c
@@ -1,1 +1,20 @@
 #include "DataStructures.h"
+
+void triNodesOccurence(Noeud** AVL){
+	if (*AVL) {
+		if ((*AVL)->sag) {
+			triNodesOccurence(&((*AVL)->sag));
+
+			if ((*AVL)->occ < (*AVL)->sag->occ) {
+				rightRotation(AVL);
+			}
+		}
+		if ((*AVL)->sad) {
+			triNodesOccurence(&((*AVL)->sad));
+
+			if ((*AVL)->occ < (*AVL)->sad->occ) {
+				leftRotation(AVL);
+			}
+		}
+	}
+}
