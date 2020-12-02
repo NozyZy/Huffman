3ecb6f1c85994a9c774d0b626f060de1d7888f84
Quentin V <quentin.vincent@efrei.net>
2020-12-02 19:32:43 +0100

    Update InteractWithFile.h

--- InteractWithFile/InteractWithFile.h
+++ InteractWithFile/InteractWithFile.h
@@ -7,7 +7,7 @@
 void error1();                              // prints the main error, for no file found
 char* loadFile(FILE* file);                 // loads the content of a .txt file, given in argument, in a char*
 void printFile(FILE* file, char* content);  // prints the content of a .txt file
-int countCharFile(FILE* file);              // returns the number of chars in a .txt file
+long unsigned int countCharFile(FILE* file);              // returns the number of chars in a .txt file
 size_t countLineFile(FILE* file);           // returns the number of chars in a .txt file
 
 #endif //HUFFMAN_INTERACTWITHFILE_H
