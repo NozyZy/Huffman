f12013be8c218287440c8773369e788caaf8c879
Quentin V <quentin.vincent@efrei.net>
2020-12-02 19:24:32 +0100

    Update DataStructures.c

--- DataStructures/DataStructures.c
+++ DataStructures/DataStructures.c
@@ -1,1 +1,35 @@
 #include "DataStructures.h"
+
+#include "DataStructures.h"
+
+void afficherListe(char* label, Element*  l) {
+    
+}
+
+void ajoutListe(Element** l, char ch) {
+
+}
+
+size_t tailleListe(Liste l){
+    return 0;
+}
+
+void freeList(Element* l) {
+    
+}
+
+Noeud* creerNoeud(char ch, size_t occ, char* bin) {
+    return NULL;
+}
+
+void afficherArbreBin(Noeud* a) {
+    
+}
+
+void freeArbre(Noeud* a) {
+    
+}
+
+void afficherTabNoeuds(Arbre* tab, size_t size, char* label){
+    
+}
