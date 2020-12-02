08c8190474dbea8aaae23474831ff3134e4c6a59
Quent <quentin.vincent@efrei.net>
2020-12-02 22:31:43 +0100

    partie 2.3 in progress... bad logic of tree, needs changements before

--- main.c
+++ main.c
@@ -2,20 +2,5 @@
 
 int main()
 {
-    FILE* fInput = fopen("../input.txt", "r+");
-    char* content = loadFile(fInput);
-    Element* l = NULL;
-    l = creerListeOccurrences(content, countCharFile(fInput));
-    fclose(fInput);
-    afficherListe("la liste", l);
-    
-    Noeud* a = NULL;
-    a = creeArbreOccurrences(l);
-    afficherArbre(a);
-
-    createDicoFile(a, "../dico.txt");
-
-    freeList(l);
-    freeArbre(a);
-    fclose(fInput);
+    return EXIT_SUCCESS;
 }
