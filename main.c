48bb2567ab0053c93b34e694ba98bb9b9bc11bd8
Quentin V <quentin.vincent@efrei.net>
2020-12-02 19:42:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 
 int main(void) {
     FILE *fInput, *fBin; // files input.txt and binary.txt
-    size_t cI, cB, ratio = 0; // charIn, charOut, and ratio cB/cI (should be 8, or 0 if empty, so integer)
+    long unsigned int cI, cB; // charIn, charOut, and ratio cB/cI (should be 8, or 0 if empty, so integer)
+    size_t ratio = 0;
     char *contentIn = NULL, *contentBin = NULL; // content of Input.txt and Output.txt
 
     fInput = fopen("../input.txt", "r+"); // <- Penser à changer le chemin d'acces !!
@@ -22,7 +23,7 @@ int main(void) {
     contentIn = loadFile(fInput);
     contentBin = loadFile(fBin);
 
-    printf("\nInput : %s\nOutput : %s\n\nInput #char : %d\nOutput #char : %d\nRatio : %d\n", contentIn, contentBin, cI, cB, ratio);
+    printf("\nInput : %s\nOutput : %s\n\nInput #char : %ld\nOutput #char : %ld\nRatio : %d\n", contentIn, contentBin, cI, cB, ratio);
 
     fclose(fInput);
     fclose(fBin);
