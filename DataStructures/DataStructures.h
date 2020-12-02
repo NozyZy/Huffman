1bc889d1bedd923b716569bcd5d0476c9582531d
Valère <61429521+ValereG@users.noreply.github.com>
2020-12-02 20:16:30 +0100

    Update DataStructures.h

--- DataStructures/DataStructures.h
+++ DataStructures/DataStructures.h
@@ -4,4 +4,30 @@
 #ifndef HUFFMAN_DATASTRUCTURES_H
 #define HUFFMAN_DATASTRUCTURES_H
 
+
+typedef struct Element {
+	char ch;
+	int occ;
+	struct Element* suivant;
+}Element;
+
+typedef struct Noeud {
+	char ch;
+	int occ;
+	char* bin;
+	struct Noeud* sad;
+	struct Noeud* sag;
+}Noeud;
+
+void addNodeBSTch(Noeud** AVL, Noeud* tmp);
+size_t depth(Noeud* a);
+int getBF(Noeud* a);
+void leftRotation(Noeud** a);
+void rightRotation(Noeud** a);
+void balance(Noeud** a);
+void addNodeAVL(Noeud** AVL, Noeud* tmp);
+void createAVLcaractere(Noeud** AVL, char* content, size_t taille);
+void triNodesOccurence(Noeud** AVL);
+void freeArbre(Noeud* a);
+
 #endif //HUFFMAN_DATASTRUCTURES_H
