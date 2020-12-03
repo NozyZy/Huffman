3ec78ce5aad56b126db01504e1b2d78b389e7f51
Quent <quentin.vincent@efrei.net>
2020-12-03 10:25:20 +0100

    partie 3.1 :white_check_mark: empty main.c and input.txt

--- main.c
+++ main.c
@@ -1,42 +1,6 @@
 #include "Huffman/Coding.h"
 
 int main(void) {
-    printf("\n\nTEST FULL :\n\n");
-    FILE * fInput = fopen("../input.txt", "r+");
-    if (!fInput) error1();
-
-    char* content = loadFile(fInput);
-    size_t size = countCharFile(fInput);
-    Noeud* AVLocc = NULL;
-
-    createAVLcaractere(&AVLocc, content, size);
-    afficherArbre(AVLocc);
-
-    fclose(fInput);
-    freeArbre(AVLocc);
-    free(content);
-
-    printf("\n\nTEST LITTLE :\n\n");
-
-    char content2[] = {'A', 'A', 'B', 'C', 'C', 'C', 'C', 'Z', '\0'};
-    int size2 = 8;
-    Noeud* AVLocc2 = NULL;
-
-    createAVLcaractere(&AVLocc2, content2, size2);
-    afficherArbre(AVLocc2);
-
-    freeArbre(AVLocc2);
-
-    printf("\n\nTEST NULL :\n\n");
-
-    char* content3 = NULL;
-    int size3 = 0;
-    Noeud* AVLocc3 = NULL;
-
-    createAVLcaractere(&AVLocc3, content3, size3);
-    afficherArbre(AVLocc3);
-
-    freeArbre(AVLocc3);
 
     return EXIT_SUCCESS;
 }
