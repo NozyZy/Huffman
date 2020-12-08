06a03589eef6a715fde0f63db38f1d224c6dcdca
Valère <61429521+ValereG@users.noreply.github.com>
2020-12-08 16:56:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,43 @@
 #include "Huffman/Coding.h"
+#if defined POSIX
+#define CLEAR system ( "clear" )
+#elif defined MSDOS || defined WIN32
+#define CLEAR system ( "cls" )
+#endif
 
 int main(void) {
     int choice = 0;
-    printf("\n--------------Compresseur de fichier Huffman---------------\n");
-    printf("\n\n   Que desirez-vous faire ?");
-    printf("\n .1 -> Compresser mon fichier 'input.txt'");
-    printf("\n .2 -> Decompresser mon fichier 'huffman.txt'");
-    printf("\n .3 -> Fermer le programme");
-    printf("\n\n ATTENTION : Tous vos fichiers seront ecrases par les nouveaux, assurez-vous de faire les sauvegardes necessaires si besoin.\n\n");
-    while (choice < 1 || choice > 3) {
-        printf(" Votre choix -> ");
-        scanf("%d", &choice);
-    }
 
-    switch (choice){
+    while (choice != 3) {
+        printf("\n--------------Compresseur de fichier Huffman---------------\n");
+        printf("\n\n   Que desirez-vous faire ?");
+        printf("\n .1 -> Compresser mon fichier 'input.txt'");
+        printf("\n .2 -> Decompresser mon fichier 'huffman.txt'");
+        printf("\n .3 -> Fermer le programme");
+        printf("\n\n ATTENTION : Tous vos fichiers seront ecrases par les nouveaux, assurez-vous de faire les sauvegardes necessaires si besoin.\n\n");
+        while (choice < 1 || choice > 3) {
+            printf(" Votre choix -> ");
+            scanf("%d", &choice);
+        }
+
+        switch (choice) {
         case (1):
             zipFile("../input.txt", "../huffman.txt");
+            system("..\\huffman.txt");
+            choice = 0;
             break;
         case (2):
             unzipFile("../dico.txt", "../huffman.txt");
+            system("..\\dico.txt");
+            choice = 0;
             break;
         default:
             printf("\nFermeture du programme...\n");
+            system("..\\output.txt");
             break;
+        }
+        system("PAUSE");
+        CLEAR;
     }
-
-    system("PAUSE");
-
     return EXIT_SUCCESS;
 }
